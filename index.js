974c985d23b4b5de791929f64e0bb08549a36e80
Brandon Teich <brandonteich123@gmail.com>
2019-01-05 02:08:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-function shout(string) {
+function shout(string) 
+{
   return string.toUpperCase()
 }
 
- function whisper(string) {
+function whisper(string) 
+{
   return string.toLowerCase()
 }
 
- function logShout(string) {
+function logShout(string) 
+{
   console.log(string.toUpperCase())
 }
 
- function logWhisper(string) {
+function logWhisper(string) 
+{
   console.log(string.toLowerCase())
 }
 
- function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
+function sayHiToGrandma(string) 
+{
+  if (string.toLowerCase() === string) 
+  {
     return "I can\'t hear you!";
   } 
-  else if (string.toUpperCase() === string) {
+  else if (string.toUpperCase() === string) 
+  {
     return "YES INDEED!";
   } 
-  else if (string === "I love you, Grandma.") {
-    return "I lovew you, too.";
+  else if (string === "I love you, Grandma.") 
+  {
+    return "I love you, too.";
   }
 } 
